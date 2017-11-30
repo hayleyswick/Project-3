92b030e07567da899ef841868e3c43e73d33963f
wi77jones <wi77jones@aol.com>
2017-11-30 13:48:00 -0500

    Add files via upload

--- src/Users/SalesAssociate.java
+++ src/Users/SalesAssociate.java
@@ -1,25 +1,142 @@
 package Users;
 
-import Application.SalesVanWarehouse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Scanner;
+
+import Application.*;
 
 public class SalesAssociate extends User {
-    static final long serialVersionUID = 8;
 
-
+	private ArrayList<SalesInvoice> invoices = new ArrayList<SalesInvoice>();
+	private ArrayList<Inventory> inventory = new ArrayList<Inventory>();
+	private SalesVanWarehouse s;
+	
     public SalesAssociate(String username, String password, String email,
-                          String firstName, String lastName, String phoneNumber) {
+                    String firstName, String lastName, String phoneNumber) {
         super(username, password, email, firstName, lastName, phoneNumber);
 
     }
-
-    public SalesAssociate(User user) {
+    public SalesAssociate(User user){
         this.setUsername(user.getUsername());
         this.setPassword(user.getPassword());
         this.setEmail(user.getEmail());
         this.setFirstName(user.getFirstName());
         this.setLastName(user.getLastName());
         this.setPhoneNumber(user.getPhoneNumber());
+
     }
+    
+    public void addSalesVan(SalesVanWarehouse s)
+    {
+    		this.s = s;
+    		inventory.addAll(s.getDB());
+    }
+    
+    public void sellToBikeShop(String bikeShopName, String signatureBy, String fileOfPartsToSell) throws FileNotFoundException
+	{
+		Calendar date = Calendar.getInstance();
+		ArrayList<Inventory>i = new ArrayList<Inventory>();
+		File f = new File(fileOfPartsToSell);
+		Scanner input = new Scanner(f);
+		int num = input.nextInt();
+		input.nextLine();
+		for(int x=0;x<num;x++)
+		{
+			String line = input.nextLine();
+			String[] elements = line.split(",");
+//			BikePart b = new BikePart(elements[0],Integer.parseInt(elements[1]),Double.parseDouble(elements[2]),Double.parseDouble(elements[3]),
+//					Boolean.parseBoolean(elements[4]));
 
+			i.add(new Inventory(new BikePart(elements[0],Integer.parseInt(elements[1]),Double.parseDouble(elements[2]),Double.parseDouble(elements[3]),
+					Boolean.parseBoolean(elements[4])),Integer.parseInt(elements[5])));
+		}
+		
+		for(Inventory a : inventory)
+			for(Inventory inv : i)
+				if(a.getBikePart().getID()==inv.getBikePart().getID())
+					a.setQuantity(a.getQuantity()-inv.getQuantity());
+		
+		SalesInvoice invoice = new SalesInvoice();
+		invoice.setSeller(this.getFirstName()+" "+this.getLastName());
+		invoice.generateSalesInvoice(i,bikeShopName,date,signatureBy);
+		System.out.println(invoice.getInvoice());
+		invoices.add(invoice);
+		
+	}
 
+//	public void loadVan(String file, ArrayList<Warehouse> w)
+//	{
+//		moveParts(file, w);
+//		s.addParts(bp);
+//	}
+	
+	public ArrayList<SalesInvoice> getInvoices()
+	{
+		return invoices;
+	}
+
+//	private void moveParts(String file, ArrayList<Warehouse> w)
+//	{
+//		try
+//		{
+//			File f = new File(file);
+//			Scanner input = new Scanner(f);
+//			String warehouseName = input.nextLine();
+//			while(input.hasNextLine())
+//			{
+//				String line = input.nextLine();
+//				String[] elements = line.split(",");
+//				String partName = elements[0];
+//				int quantity = Integer.parseInt(elements[1]);
+//				for(Warehouse wh : w)
+//				{
+//					if(wh.getName().equals(warehouseName))
+//					{
+//						for(int x=0;x<wh.getSize();x++)
+//						{
+//							if(wh.getPartName(x).equals(partName))
+//							{
+//								bp.add(new BikePart(wh.getPartName(x),wh.getPartNumber(x),wh.getPartPrice(x),wh.getPartSalePrice(x),
+//										wh.getPartOnSale(x),quantity));
+//								break;
+//							}
+//						}
+//						for(int x=0;x<quantity;x++)
+//						{
+//							wh.sellPartByName(partName);
+//						}
+//					}
+//				}
+//			}
+//		}
+//		catch(FileNotFoundException e)
+//		{
+//			System.out.println(e);
+//		}
+//	}
+	
+	public void writeInvoices()
+	{
+		try 
+		{
+			Collections.sort(invoices);
+			ObjectOutputStream objOutputStream = new ObjectOutputStream(new FileOutputStream("invoices.bin"));
+			objOutputStream.writeObject(invoices);
+		}   
+		catch (FileNotFoundException e1) 
+		{
+			e1.printStackTrace();
+		}
+		catch (IOException e1) 
+		{
+			e1.printStackTrace();
+		}
+	}
 }
