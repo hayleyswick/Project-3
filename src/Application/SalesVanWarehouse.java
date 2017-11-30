49c38421285e7c9b9fe0bac730e27450907dd24a
wi77jones <wi77jones@aol.com>
2017-11-30 16:14:26 -0500

    Update SalesVanWarehouse.java
    Added movePart method

--- src/Application/SalesVanWarehouse.java
+++ src/Application/SalesVanWarehouse.java
@@ -3,37 +3,41 @@ package Application;
 import Users.SalesAssociate;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class SalesVanWarehouse extends Warehouse implements Serializable {
-	
+
 
 	private String name;
-	private SalesAssociate operator;
 	static final long serialVersionUID = 34567;
-	
-	public SalesVanWarehouse(String n, SalesAssociate operator) {
+
+	public SalesVanWarehouse(String n) {
 		this.setName(n);
-		this.setOperator(operator);
 	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public SalesAssociate getOperator() {
-        return operator;
-    }
-
-    public void setOperator(SalesAssociate operator) {
-        this.operator = operator;
-    }
-
-    public String getName() {
+	public String getName() {
 		return name;
 	}
-	
-	
+
+	public void movePart(Warehouse w, String partName, int quantity)
+	{
+		ArrayList<Inventory>wI = new ArrayList<Inventory>();
+		wI.addAll(w.getDB());
+		for(Inventory i1 : wI)
+		{
+			if(partName.equals(i1.getBikePart().getName()))
+			{
+				this.addPart(i1.getBikePart(),quantity);
+				i1.setQuantity(i1.getQuantity()-quantity);
+			}
+		}
+	}
+
 	public void sellPart(long id, long x) {
 		Iterator<Inventory> it = dbIterator();
 		while (it.hasNext()) {
@@ -42,9 +46,9 @@ public class SalesVanWarehouse extends Warehouse implements Serializable {
 				long quantity = inventory.getQuantity();
 				updateInventory(inventory, quantity-x, true);
 			}
-				
+
 		}
-		
+
 	}
 
 }
